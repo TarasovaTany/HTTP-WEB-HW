48fb5a45f798bced81898b8b5d3b8e123ec8e9e2
Tatyana Tarasova <umnova.panther@yandex.ru>
2024-08-07 16:25:33 +0500

    Changing the classes Handler, Server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.io.IOException;
+
 public class Main {
     private static final int SERVER_PORT = 9999;
     private static final int THREAD_POOL_SIZE = 64;
 
     public static void main(String[] args) {
         Server server = new Server(SERVER_PORT, THREAD_POOL_SIZE);
+
+        // добавление хендлеров (обработчиков)
+        server.addHandler("GET", "/messages", (request, responseStream) -> {
+            try {
+                server.responseWithoutContent(responseStream, "404", "Not found");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        server.addHandler("POST", "/messages", (request, responseStream) ->
+                server.responseWithoutContent(responseStream, "503", "Service Unavailable"));
+
+        server.addHandler("GET", "/messages", (request, outputStream) ->
+                server.responseWithoutContent(outputStream, "index.html"));
+
         server.start();
     }
 }
+
